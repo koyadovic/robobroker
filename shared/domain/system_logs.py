adadb18ba479edf081025ceaeff21699811a99a3
Miguel Collado <m.collado.gomez@gmail.com>
2021-02-03 18:47:42 +0100

    Actualizar system_logs.py

--- shared/domain/system_logs.py
+++ shared/domain/system_logs.py
@@ -1,6 +1,5 @@
-from shared.application.models import DSystemLog
-
 
 def add_system_log(log_type: str, text):
     # TODO enhance this
+    from shared.application.models import DSystemLog
     DSystemLog.objects.create(log_type=log_type, text=text)
