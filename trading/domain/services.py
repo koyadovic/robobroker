8664c8b4174b60a77217108c9cabe523e477e5f5
Miguel Collado <m.collado.gomez@gmail.com>
2021-02-03 11:41:39 +0100

    Actualizar services.py

--- trading/domain/services.py
+++ trading/domain/services.py
@@ -1,10 +1,14 @@
-import math
-from typing import List
+from datetime import datetime, timedelta
 
 from shared.domain.dependencies import dependency_dispatcher
 from trading.domain.entities import Cryptocurrency, Package
 from trading.domain.interfaces import ILocalStorage, ICryptoCurrencySource
 from trading.domain.tools import profit_difference_percentage
+
+from typing import List
+
+import statistics
+import math
 
 COMMON_CURRENCY = 'EUR'
 
@@ -17,7 +21,9 @@ def trade():
     3.- Se finaliza llamando check_purchase(candidates_for_purchase)
     """
     for_sell, for_purchase = _discriminate_by_sell_and_purchase()
-    pass
+    for currency in for_sell:
+        _check_sell(currency)
+    _check_buy(for_purchase)
 
 
 def _discriminate_by_sell_and_purchase():
@@ -32,23 +38,31 @@ def _discriminate_by_sell_and_purchase():
 
     trading_cryptocurrencies = trading_source.get_trading_cryptocurrencies()
     for currency in trading_cryptocurrencies:
-        # TODO analyze tendencies
-        """
-        Crecimiento 7 días      Crecimiento 1 día       qué hacer
-        +                       +                       Si no tienes nada, comprar.
-        +                       -                       Mirar si vender
-        -                       +                       Mirar si comprar
-        -                       -                       nada.
-        """
+        now = datetime.utcnow()
+        prices = trading_source.get_last_month_prices(currency)
+        last_6h_prices = [price for price in prices if now - timedelta(hours=6) <= price.instant]
 
-        """
-        si mean(to_profit(last_week)) > 0 y mean(to_profit(last_6_hours)) < 0, candidata a vender!
-        si mean(to_profit(last_week)) < 0 y mean(to_profit(last_6_hours)) > 0, candidata a comprar!
-        si mean(to_profit(last_week)) > 0 y mean(to_profit(last_6_hours)) > 0, y no tienes nada, candidata a comprar!
-        
-        Cuidado!! quizá tendría que ser mean(to_profit(last_week)) + standard deviation!
-        """
-        data = trading_source.get_last_month_prices(currency, COMMON_CURRENCY)
+        # Si el precio actual está por encima del precio mediano + stdev último mes,
+        # y la rentabilidad 6h es < 0, vender!
+        current_sell_price = trading_source.get_current_sell_price(currency)
+        sell_prices = [price.sell_price for price in prices]
+        sell_prices_median = statistics.median(sell_prices)
+        sell_prices_stdev = statistics.stdev(sell_prices)
+        last_6h_profit = profit_difference_percentage(last_6h_prices[0].sell_price, last_6h_prices[-1].sell_price)
+        if current_sell_price > sell_prices_median + sell_prices_stdev and last_6h_profit < 0:
+            for_sell.append(currency)
+            continue
+
+        # Si el precio actual está por debajo del precio mediano - stdev último mes,
+        # y la rentabilidad 6h es > 0, comprar!
+        current_buy_price = trading_source.get_current_buy_price(currency)
+        buy_prices = [price.buy_price for price in prices]
+        buy_prices_median = statistics.median(buy_prices)
+        buy_prices_stdev = statistics.stdev(buy_prices)
+        last_6h_profit = profit_difference_percentage(last_6h_prices[0].buy_price, last_6h_prices[-1].buy_price)
+        if current_buy_price < buy_prices_median - buy_prices_stdev and last_6h_profit > 0:
+            for_purchase.append(currency)
+            continue
 
     # TODO si tenemos currencies por un determinado tiempo que no vendemos y que no sacamos rentabilidad
     #  tocaría vender sí o sí.
@@ -63,8 +77,10 @@ def _check_sell(candidate_currency: Cryptocurrency):
     packages = storage.get_cryptocurrency_packages(candidate_currency)
     amount = 0.0
 
+    current_sell_price = trading_source.get_current_sell_price(candidate_currency)
+
     for package in packages:
-        profit_difference = profit_difference_percentage(package.bought_at_price, candidate_currency.sell_price)
+        profit_difference = profit_difference_percentage(package.bought_at_price, current_sell_price)
         if profit_difference > 10.0:
             amount += package.currency_amount
 
@@ -81,13 +97,19 @@ def _check_buy(candidate_currencies: List[Cryptocurrency]):
     source_cryptocurrency = trading_source.get_stable_cryptocurrency()
     # source amount is amount of this currency
     source_amount = trading_source.get_amount_owned(source_cryptocurrency)
-
+    if round(source_amount) <= 1:
+        return
     parts = len(candidate_currencies)
-    # queremos diversificación, no jugárnosla a una única moneda
+    # queremos diversificación, no jugárnosla convirtiendo all a una única moneda
     if parts < 3:
         parts = 3
     for target_currency in candidate_currencies:
         source_fragment_amount = math.floor((source_amount / parts) * 100.0) / 100.0
         trading_source.convert(source_cryptocurrency, source_fragment_amount, target_currency)
-        storage.save_package(Package())
-        # TODO crear el package
+        target_currency_buy_price = trading_source.get_current_buy_price(target_currency)
+        storage.save_package(Package(
+            currency_symbol=target_currency.symbol,
+            currency_amount=source_fragment_amount,
+            bought_at_price=target_currency_buy_price,
+            operation_datetime=datetime.utcnow(),
+        ))
