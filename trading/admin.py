0511d7b702720ca3aed3bffe0550a455723a6a9a
Miguel Collado <m.collado.gomez@gmail.com>
2021-02-03 12:59:03 +0100

    Actualizar admin.py, django_admin.py y 2 más archivos...

--- trading/admin.py
+++ trading/admin.py
@@ -1,3 +1,1 @@
-from django.contrib import admin
-
-# Register your models here.
+from trading.application.django_admin import *
--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,1 @@
-from django.db import models
-
-# Create your models here.
+from trading.application.django_models import *
