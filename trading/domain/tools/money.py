02981fd18fcbe96196b66a446088028b0152dd80
Miguel Collado <m.collado.gomez@gmail.com>
2021-02-08 18:06:22 +0100

    Actualizar money.py

--- trading/domain/tools/money.py
+++ trading/domain/tools/money.py
@@ -5,3 +5,9 @@ def two_decimals_floor(num):
     return '{:.2f}'.format(
         math.floor(num * 100.0) / 100.0
     )
+
+
+def eight_decimals_floor(num):
+    return '{:.2f}'.format(
+        math.floor(num * 100000000.0) / 100000000.0
+    )
