a377dc6534151afd3cb6579e0152020d834eb874
Miguel Collado <m.collado.gomez@gmail.com>
2021-02-08 20:41:14 +0100

    Actualizar local.py

--- robobroker/settings/local.py
+++ robobroker/settings/local.py
@@ -1,1 +1,15 @@
 from .base import *
+
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://2fe387acbc414f0d9954e78b3da1535c@o230030.ingest.sentry.io/5621802",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
