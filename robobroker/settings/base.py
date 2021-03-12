101ea6967283e88a3dee429927e7869b36fd8e45
Miguel Collado <m.collado.gomez@gmail.com>
2021-03-12 07:24:40 +0100

    Actualizar base.py y prod.py

--- robobroker/settings/base.py
+++ robobroker/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 import sys
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(tfbkbzefw81uf4i6$+-ocqtz+^$(3*hdh&ts5^%y672vgj9)o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
